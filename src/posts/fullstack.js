f54c567cb24435bc7ce68ba3d7ed5b58355bb7f2
cclintris <181250083@smail.nju.edu.cn>
2023-08-31 01:44:54 -0700

    add: update cclinsing link and 1 blog

---
+++ src/posts/fullstack.js
@@ -0,0 +1,9 @@
+const fullStackPosts = [
+  {
+    title: "[Backend]: HTTP Token - Basic Token and Bearer Token",
+    url: "https://snippets.cacher.io/snippet/01d150a5d7dc404e0eea",
+    date: "Aug 29, 2023",
+  },
+];
+
+export default fullStackPosts;
